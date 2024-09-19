0ade7e11665f039818a3fc61e731a1a282945c17
jean TROUSSIER <jean.troussier@cpe.fr>
2024-09-19 11:54:36 +0200

    fianl project

--- fonctioncalculmatriciel.py
+++ fonctioncalculmatriciel.py
@@ -3,3 +3,47 @@ user : Jean TROUSSIER
 Date : Jeudi 19 septembre
 Obj : multiplication matriciel (fichier principal)
 """
+def Are_they_matrix(mat):
+    """
+    fonction qui verifie que les matrices (liste ici) ait le même nombre d'élément sur chaque ligne
+    """
+    try :
+        valligne = len(mat[0])
+    except:
+        return False
+    for i in mat:
+        try :
+            if valligne != len(i) : 
+                return False
+            for j in i:
+                int(j)
+        except:
+            return False
+    return True
+
+
+
+def mult_possible(MatA,MatB):
+    """
+    fonction qui reçoit deux matrice et qui renvoie true si les matrice sont divisible entre elle sinon false
+    """
+    if len(MatA) != len(MatB):
+        return False
+    return True
+
+def multiplication(MatA, MatB):
+    """
+    ce programme prend en entrée deux matrice a multiplier et return la dite matrice 
+    """
+    resultat_matrice = []
+    conteneur = []
+    number = 0
+    for i in range(len(MatA)):
+        for k in range(len(MatB[0])):
+            for j in range(len(MatA[0])):
+                number += MatA[i][j] * MatB[j][k]
+            conteneur.append(number)
+            number = 0
+        resultat_matrice.append(conteneur[:])
+        conteneur = []
+    return resultat_matrice
--- mainprogram.py
+++ mainprogram.py
@@ -3,3 +3,15 @@ user : Jean TROUSSIER
 Date : Jeudi 19 septembre
 Obj : multiplication matriciel (fichier principal)
 """
+import fonctioncalculmatriciel as fcm
+a = [[4,5,6],[5,6,8],[7,8,9]]
+b = [[8,2,4],[9,1,2],[]]
+flag = 0
+if fcm.Are_they_matrix(a) == False or fcm.Are_they_matrix(b) ==False:
+    print ("non matrix array")
+    flag = 1
+if fcm.mult_possible(a,b) == False:
+    print("non multiplicative matrix")
+    flag = 1
+if flag != 1 :
+    print(fcm.multiplication(a,b))
